126a24e6ff4786798cc815b1d9913043138fb853
sudoaaditya <kashidadi@gmail.com>
2025-05-14 19:53:56 +0530

    fractal tree init

---
+++ fractal-tree/src/components/Sketch.js
@@ -0,0 +1,133 @@
+import * as THREE from 'three';
+// import { OrbitControls } from 'three/examples/jsm/Addons.js';
+
+import GUI from 'lil-gui';
+
+
+class Sketch {
+
+    constructor(container) {
+        this.container = container;
+
+        // threejs vars
+        this.scene = null;
+        this.renderer = null;
+        this.camera = null;
+        // this.controls = null;
+
+        this.sizes = {};
+        this.frameId = null;
+        this.clock = null;
+        this.gui = new GUI();
+
+        this.initialize();
+    }
+
+    initialize = () => {
+
+        this.texLoader = new THREE.TextureLoader();
+
+        this.scene = new THREE.Scene();
+
+        this.sizes.width = window.innerWidth;
+        this.sizes.height = window.innerHeight;
+
+        this.renderer = new THREE.WebGLRenderer({
+            antialias: true,
+            canvas: this.container
+        });
+        this.renderer.setSize(this.sizes.width, this.sizes.height);
+        this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+        this.renderer.setClearColor(0x000000, 1);
+        this.renderer.outputColorSpace = THREE.SRGBColorSpace;
+
+        this.clock = new THREE.Clock();
+
+        // camera & resize
+        this.setupCamera();
+        this.setupResize();
+
+        // wramup calls
+        this.resize();
+        this.render();
+
+        // world setup
+        this.settings()
+        this.addContents();
+
+        // start animation loop
+        this.start();
+    }
+
+    settings = () => {
+        this.settings = {
+        };
+    }
+
+    setupCamera = () => {
+
+        this.camera = new THREE.PerspectiveCamera(
+            35,
+            (this.sizes.width / this.sizes.height),
+            0.1,
+            1000
+        );
+
+        this.camera.position.set(0, 0, 10);
+
+        // this.controls = new OrbitControls(this.camera, this.renderer.domElement)
+    }
+
+    setupResize = () => {
+        window.addEventListener('resize', this.resize);
+    }
+
+
+    resize = () => {
+        this.sizes.width = window.innerWidth;
+        this.sizes.height = window.innerHeight;
+
+        this.camera.aspect = this.sizes.width / this.sizes.height;
+        this.camera.updateProjectionMatrix();
+
+        this.renderer.setSize(this.sizes.width, this.sizes.height)
+    }
+
+    start = () => {
+        if (!this.frameId) {
+            this.frameId = window.requestAnimationFrame(this.update);
+        }
+    }
+
+    stop = () => {
+        cancelAnimationFrame(this.frameId);
+    }
+
+    addContents = () => {
+        // render base scene data!
+        this.material = new THREE.MeshBasicMaterial({color: 'red', side: THREE.DoubleSide});
+
+        this.geometry = new THREE.PlaneGeometry(5, 5);
+
+        this.plane = new THREE.Mesh(this.geometry, this.material);
+        this.scene.add(this.plane);
+
+    }
+
+    update = () => {
+        this.elpasedTime = this.clock.getElapsedTime();
+
+        this.render();
+
+        this.frameId = window.requestAnimationFrame(this.update);
+    }
+
+    render = () => {
+        let { renderer, scene, camera, } = this;
+        if (renderer) {
+            renderer.render(scene, camera);
+        }
+    }
+}
+
+export { Sketch };
